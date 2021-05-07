d5d47bfbd0f8414ca080ee70137264225b9e8b4e
satyaCsgithub <satya_inet@yahoo.com>
2021-05-07 23:30:39 +0530

    .gitignore is customized

---
+++ Second.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmpDetail
+    {
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+        public string Location { get; set; }
+        
+        public string NativeID { get; set; }
+
+        public string SatyaDB { get; set; }
+
+        Public string GitIgnore { get; set; }
+    }
+}
